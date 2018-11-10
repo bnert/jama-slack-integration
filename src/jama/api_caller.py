33e6b10035f9c5a81c50879da15ee512a447687e
Jingyu Ye <jingyu2@pdx.edu>
2018-11-09 17:22:54 -0800

    Using database setting to choose Jama OAuth or bot user

--- src/jama/api_caller.py
+++ src/jama/api_caller.py
@@ -1,4 +1,5 @@
 from jama import oauth
+import os
 import json
 import requests
 
@@ -11,14 +12,17 @@ def post(team_id, user_id, url, payload):
         url -> Jama API url
         payload -> The dictionary of payload wants to be sent to the url
     """
-    token = oauth.get_access_token(team_id, user_id)
-    if token is None:
-        return None
-    header_list = {
-        "content-type": "application/json",
-        "Authorization": "Bearer " + token
-    }
-    response = requests.post(url, json=payload, headers=header_list)
+    if "DB_HOST" in os.environ:
+        token = oauth.getAccessToken(team_id, user_id)
+        if token is None:
+            return None
+        header_list = {
+            "content-type": "application/json",
+            "Authorization": "Bearer " + token
+        }
+        response = requests.post(url, json=payload, headers=header_list)
+    else:
+        response = requests.post(url, json=payload, auth=(os.environ["JAMA_USER"], os.environ["JAMA_PASS"]))
     return json.loads(response.text)
 
 def get(team_id, user_id, url):
@@ -29,14 +33,17 @@ def get(team_id, user_id, url):
         user_id -> Slack User ID
         url -> Jama API url
     """
-    token = oauth.get_access_token(team_id, user_id)
-    if token is None:
-        return None
-    header_list = {
-        "content-type": "application/json",
-        "Authorization": "Bearer " + token
-    }
-    response = requests.get(url, headers=header_list)
+    if "DB_HOST" in os.environ:
+        token = oauth.getAccessToken(team_id, user_id)
+        if token is None:
+            return None
+        header_list = {
+            "content-type": "application/json",
+            "Authorization": "Bearer " + token
+        }
+        response = requests.get(url, headers=header_list)
+    else:
+        response = requests.get(url, auth=(os.environ["JAMA_USER"], os.environ["JAMA_PASS"]))
     return json.loads(response.text)
 
 def put_file(team_id, user_id, url, file_name, file_data):
@@ -49,8 +56,11 @@ def put_file(team_id, user_id, url, file_name, file_data):
         file_name -> The file name of the file
         file_data -> The file data wants to be sent to the url
     """
-    token = oauth.get_access_token(team_id, user_id)
-    if token is None:
-        return None
-    response = requests.put(url, files={"file": (file_name, file_data)}, headers={"Authorization": "Bearer " + token})
+    if "DB_HOST" in os.environ:
+        token = oauth.getAccessToken(team_id, user_id)
+        if token is None:
+            return None
+        response = requests.put(url, files={"file": (file_name, file_data)}, headers={"Authorization": "Bearer " + token})
+    else:
+        response = requests.put(url, files={"file": (file_name, file_data)}, auth=(os.environ["JAMA_USER"], os.environ["JAMA_PASS"]))
     return response.status_code

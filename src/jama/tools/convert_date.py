e8c8e89cbd83085a1f60a3e88f5ec719bae6f59f
Bonden Lyons <bonden@pdx.edu>
2018-11-26 23:45:02 -0800

    Removed unused library. Moved function docstring header.

--- src/jama/tools/convert_date.py
+++ src/jama/tools/convert_date.py
@@ -1,15 +1,13 @@
-from flask import make_response
 import time
 from dateutil.parser import parse
 
-"""
-Function produces and sends error message to user.
-
-Attributes:
-    None
-"""
-#TODO
-
-def to_epoch_time(date):
+def convert(date):
+    """
+    Function will convert date to a human friendly format.
+    Args:
+       date (string): The unfriendly date string to be parsed 
+    Returns:
+       (string): The human friendly date string produced by the time objects timestamp method 
+    """
     date_time = parse(date)
     return date_time.timestamp()
